69acbb2491125a00c8b0e70395da6f22b4f9174a
Ayush Kumar <ayushkumar28082004@gmail.com>
2023-03-21 23:25:55 +0530

    linear search

---
+++ linearSearch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool search(int arr[],int size,int element){
+    for(int i=0;i<size;i++){
+        if(arr[i]==element){
+            return true;
+        }
+       
+    }
+return false;
+}
+
+int main(){
+        int arr[1000];
+    int size;
+    cin>>size;
+    int n;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    cout<<"enter the element to search"<<endl;
+    cin>>n;
+    bool found = search(arr,size,n);
+    if(found){
+        cout<<"present";
+    }
+    else{
+        cout<<"not present";
+    }
+}
