58ff09dd7a8dc179940a85f1aacf81669f7a7253
Ayush Kumar <ayushkumar28082004@gmail.com>
2023-03-26 23:58:18 +0530

    binarysearch

---
+++ reverseAlternateElements.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void swapAlternate(int arr[], int size)
+{
+
+    
+ 
+    for (int i = 0; i < size; i+=2)
+    {
+
+        while (i+1 < size)
+        {
+            swap(arr[i], arr[i+1]);
+            
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+
+    int even[8] = {5, 4, 8, 9, 6, 5, 4,10};
+
+    swapAlternate(even, 8);
+    printArray(even, 8);
+
+    cout<<"done";
+}
---
+++ sort01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void printarray(int arr[],int n){
+
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+cout<<endl;
+}
+
+void sortone(int arr[], int n){
+    int i=0;
+    int j = n-1;
+    
+    while(i<j){
+        if(arr[i]==0){
+            i++;
+        }
+        else if(arr[j]==1){
+            j--;
+        }
+        else if(arr[i]==1&& arr[j]==0){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+}
+int main(){
+    int arr[9] = {0,0,1,0,1,0,1,0,1};
+    sortone(arr,9);
+    printarray(arr,9);
+}
