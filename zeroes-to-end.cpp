39b2397afc91e997f11c274ec2b9dcada580e28e
ayushkumar2808 <ayushkumar28082004@gmail.com>
2024-02-01 11:06:39 +0530

    test commit

--- zeroes-to-end.cpp
+++ zeroes-to-end.cpp
@@ -9,35 +9,35 @@ int main(){
 
   // BRUTE FORCE
 
-  // for(int i=0;i<10;i++){
-  //   if(arr[i]!=0){
-  //     temp[index] = arr[i];
-  //     index++;
-  //   }else{
-  //     zerocount++;
-  //   }
-  // }
+  for(int i=0;i<10;i++){
+    if(arr[i]!=0){
+      temp[index] = arr[i];
+      index++;
+    }else{
+      zerocount++;
+    }
+  }
 
-  // for(int i=index;i<10;i++){
-  //   temp[i]=0;
+  for(int i=index;i<10;i++){
+    temp[i]=0;
 
-  // }
-  // for(int i=0;i<10;i++){
-  //   cout<<temp[i]<<" ";
-  // }
+  }
+  for(int i=0;i<10;i++){
+    cout<<temp[i]<<" ";
+  }
 
 
 
   // OPTIMAL
-  int j=0;
+  // int j=0;
   
-  for(int i=0;i<10;i++){
-    if(arr[i]!=0){
-      swap(arr[i],arr[j]);
-      j++;
-    }
-  }
-    for(int i=0;i<10;i++){
-    cout<<arr[i]<<" ";
-  }
+  // for(int i=0;i<10;i++){
+  //   if(arr[i]!=0){
+  //     swap(arr[i],arr[j]);
+  //     j++;
+  //   }
+  // }
+  //   for(int i=0;i<10;i++){
+  //   cout<<arr[i]<<" ";
+  // }
 }
